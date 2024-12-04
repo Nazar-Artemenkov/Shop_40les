a30564b4d61a4cfea7ddc2a12a14527adc087439
Nazar Artemenkov <bima62374@gmail.com>
2024-12-04 02:57:34 +0200

    Domain layer implemented

---
+++ src/main/java/app/domain/Product.java
@@ -0,0 +1,60 @@
+package app.domain;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String title;
+    private double prise;
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrise() {
+        return prise;
+    }
+
+    public void setPrise(double prise) {
+        this.prise = prise;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(prise, product.prise) == 0 && active == product.active && Objects.equals(id, product.id) && Objects.equals(title, product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, prise, active);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product: id - %d, title - %s, price - %.2f, active - %s.",
+                id, title, prise, active ? "yes":"no");
+    }
+}
