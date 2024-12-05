c63a866d83e96ab00aab92856cb4135ee21d2157
Nazar Artemenkov <bima62374@gmail.com>
2024-12-06 01:04:47 +0200

    Repository layer implemented

--- src/main/java/app/repository/ProductRepositoryImpl.java
+++ src/main/java/app/repository/ProductRepositoryImpl.java
@@ -2,31 +2,65 @@ package app.repository;
 
 import app.domain.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepositoryImpl implements ProductRepository {
+
+    private final List<Product> database = new ArrayList<>();
+    private long currentId = 0;
+
     @Override
     public Product save(Product product) {
-        return null;
+        product.setId(++currentId);
+        database.add(product);
+        return product;
     }
 
     @Override
     public List<Product> findAll() {
-        return List.of();
+        return database;
     }
 
     @Override
     public Product findById(Long id) {
-        return null;
+        return database.stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public void update(Product product) {
-
+        Long id = product.getId();
+        double newPrise = product.getPrise();
+        Product existedProduct = findById(id);
+        if (existedProduct != null){
+            existedProduct.setPrise(newPrise);
+        }
     }
 
     @Override
     public void removeById(Long id) {
-
+        database.removeIf(x -> x.getId().equals(id));
     }
+
+//    public static void main(String[] args) {
+//        ProductRepository repository = new ProductRepositoryImpl();
+//        repository.save(new Product("Banana", 120, true));
+//        repository.save(new Product("Apple", 180, true));
+//        repository.save(new Product("Peach", 90, true));
+//
+//        repository.findAll().forEach(x -> System.out.println(x));
+//        System.out.println();
+//        System.out.println(repository.findById(2L));
+//        System.out.println(repository.findById(5L));
+//        System.out.println();
+//        repository.update(new Product(2L, 95));
+//        System.out.println(repository.findById(2L));
+//
+//        System.out.println();
+//        repository.removeById(2L);
+//        repository.findAll().forEach(x -> System.out.println(x));
+//    }
 }
